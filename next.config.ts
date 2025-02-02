5a93234a2f0746ad9002a54fa2c53e737b92f323
Kane Li <122249106+Li-Kane@users.noreply.github.com>
2025-02-01 19:20:57 -0800

    custom image loader

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   basePath: "/portfolio-website",
   output: "export",
   reactStrictMode: true,
+  images:{
+    loader:"custom",
+    loaderFile:"/path/to/custom-image-loader.js",
+  },
 };
 
 export default nextConfig;
