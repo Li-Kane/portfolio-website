0a5969691671f482a3029c8828c138bef1408103
Kane Li <122249106+Li-Kane@users.noreply.github.com>
2025-02-01 19:27:17 -0800

    base path

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   basePath: "/portfolio-website",
   output: "export",
   reactStrictMode: true,
-  images:{
-    loader:"custom",
-    loaderFile:"/path/to/custom-image-loader.js",
-  },
 };
 
 export default nextConfig;
--- src/custom-image-loader.ts
+++
@@ -1,9 +0,0 @@
-import basePath from "../next.config";
-import path from "path";
-
-export default function myImageLoader({src, width}: {src: string, width: number}) {
-  if( basePath && path.isAbsolute(src) ){
-    return `${basePath}${src}?width=${width}`;
-  }
-  return `${src}?width=${width}`;
-}
